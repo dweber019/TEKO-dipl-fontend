2ff4602bcf129a712527ca3465edfc20ce51cc58
David Weber <david.weber@w3tec.ch>
2017-09-28 10:31:43 +0200

    Implement chat detail page

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+
+import { ChatDetailPage } from './../pages';
 
 /**
  * Generated class for the ChatPage page.
@@ -15,11 +17,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChatPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChatPage');
+  }
+
+  public goToChat(): void {
+    this.navCtrl.push(ChatDetailPage, 'Data from ChatPage');
   }
 
 }
--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -4,3 +4,6 @@ export const FirstRunPage = 'TutorialPage';
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
 export const MainPage = 'DashboardPage';
+
+// Chat pages
+export const ChatDetailPage = 'ChatDetailPage';
