eec8636f3e880757ac254d0f3eee0c9a92b6f6d1
David Weber <david.weber@w3tec.ch>
2017-10-16 11:18:18 +0200

    Refresh person and group list if a new one is created

--- src/components/address-person/address-person.ts
+++ src/components/address-person/address-person.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NgRadio } from 'ng-radio';
 
 import { AddressPersonDetailPage } from './../../pages/pages';
 import { UserProvider, User } from './../../providers/api-services/users';
@@ -15,21 +16,27 @@ export class AddressPersonComponent {
 
   constructor(
     private navCtrl: NavController,
-    private userProvider: UserProvider
+    private userProvider: UserProvider,
+    private ngRadio: NgRadio,
   ) {
+    this.ngRadio.on('person:*').subscribe(() => this.loadPersons());
   }
 
   public ngOnInit(): void {
+    this.loadPersons();
+  }
+
+  public goToDetail(user: User): void {
+    this.navCtrl.push(AddressPersonDetailPage, user);
+  }
+
+  private loadPersons(): void {
     this.loading = true;
     this.userProvider.getAll()
       .subscribe(data => {
         this.loading = false;
         this.users = data;
       });
-  }
-
-  public goToDetail(user: User): void {
-    this.navCtrl.push(AddressPersonDetailPage, user);
   }
 
 }
